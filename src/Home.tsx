22e113cb8ec46ac63035ac7918a19ddf69edc795
luis mata <mata.perez.luis@gmail.com>
2022-06-11 13:55:59 -0300

    primer commit

--- src/Home.tsx
+++ src/Home.tsx
@@ -650,12 +650,12 @@ const Home = (props: HomeProps) => {
                 <MintContainer>
                     <DesContainer>
                         <NFT elevation={3}>
-                            <h2>My NFT</h2>
+                            <img className="fondo"  src="POPULART-LOGO.png"/>
                             <br/>
                             <div><Price
                                 label={isActive && whitelistEnabled && (whitelistTokenBalance > 0) ? (whitelistPrice + " " + priceLabel) : (price + " " + priceLabel)}/><Image
-                                src="cool-cats.gif"
-                                alt="NFT To Mint"/></div>
+                                src="populart.gif"
+                                alt="populart"/></div>
                             <br/>
                             {wallet && isActive && whitelistEnabled && (whitelistTokenBalance > 0) && isBurnToken &&
                               <h3>You own {whitelistTokenBalance} WL
